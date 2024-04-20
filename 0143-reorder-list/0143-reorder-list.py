b661b1e5615277f7ef6968e551cfb1e647aa7d76
jay-0249 <63956291+jay-0249@users.noreply.github.com>
2024-04-20 18:36:51 -0500

    Time: 58 ms (74.2%), Space: 31.8 MB (13.35%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -10,34 +10,34 @@ class Solution(object):
         :rtype: None Do not return anything, modify head in-place instead.
         """
         if head is None or head.next is None or head.next.next is None:
-                    return head
-
-        # Find the middle of the list
+            return head
+        #Find the middle node (odd no. of nodes) or second middle node (even no.  of nodes)
         slowPointer, fastPointer = head, head
-        while fastPointer.next and fastPointer.next.next:
+        while fastPointer is not None and fastPointer.next is not None:
             slowPointer = slowPointer.next
             fastPointer = fastPointer.next.next
 
-        # Reverse the second half of the list
+        #Reverse the list after the middle or 2nd middle node
         currentNode = slowPointer.next
         prev = None
+
         while currentNode:
             nextNode = currentNode.next
             currentNode.next = prev
             prev = currentNode
             currentNode = nextNode
-
-        # Detach the second half
+        #Now turn the middle or second middle node as the Last node by assigning slowPointer.next with None
         slowPointer.next = None
         secondHalf = prev
-
-        # Merge the two halves
+        
+        #Now start inserting Ln between L0 and L1 and continue inserting the nodes from array until the second Pointer is None
         currentNode = head
         while secondHalf:
-            temp = secondHalf.next
-            secondHalf.next = currentNode.next
-            currentNode.next = secondHalf
-            currentNode = secondHalf.next
-            secondHalf = temp
-
+            lastNode = secondHalf
+            secondHalf = secondHalf.next
+            firstHalfNext = currentNode.next
+            lastNode.next = firstHalfNext
+            currentNode.next = lastNode
+            currentNode = firstHalfNext
         return head
+
