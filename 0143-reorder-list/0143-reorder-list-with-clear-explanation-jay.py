6a130250c9ea1fd0d6f14dfbd1d34933cb0896ea
jay-0249 <63956291+jay-0249@users.noreply.github.com>
2024-04-20 18:17:57 -0500

    Time: 58 ms (74.2%), Space: 31.8 MB (13.35%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list-with-clear-explanation-jay.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        if head is None or head.next is None or head.next.next is None:
+            return head
+        #Find the middle node (odd no. of nodes) or second middle node (even no.  of nodes)
+        slowPointer, fastPointer = head, head
+        while fastPointer is not None and fastPointer.next is not None:
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next
+
+        #Reverse the list after the middle or 2nd middle node
+        currentNode = slowPointer.next
+        prev = None
+
+        while currentNode:
+            nextNode = currentNode.next
+            currentNode.next = prev
+            prev = currentNode
+            currentNode = nextNode
+        #Now turn the middle or second middle node as the Last node by assigning slowPointer.next with None
+        slowPointer.next = None
+        secondHalf = prev
+        
+        #Now start inserting Ln between L0 and L1 and continue inserting the nodes from array until the second Pointer is None
+        currentNode = head
+        while secondHalf:
+            lastNode = secondHalf
+            secondHalf = secondHalf.next
+            firstHalfNext = currentNode.next
+            lastNode.next = firstHalfNext
+            currentNode.next = lastNode
+            currentNode = firstHalfNext
+        return head
+
