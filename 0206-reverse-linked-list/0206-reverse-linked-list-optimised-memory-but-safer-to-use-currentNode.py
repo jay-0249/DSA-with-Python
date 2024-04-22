d78d6c38371f37d3dcc6583b901f5b5c8f568086
jay-0249 <63956291+jay-0249@users.noreply.github.com>
2024-04-22 14:57:08 -0500

    Time: 19 ms (50.27%), Space: 13.8 MB (17.47%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list-optimised-memory-but-safer-to-use-currentNode.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        """
+        prev -> current -> next
+        prev <- current, next
+        Now move to next node, assign prevNode = currentNode,
+                                      current = nextNode
+        start with prevNode as None and currentNode as head continue until currentNode is None
+
+        """
+        prevNode = None
+        currentNode = head
+        while currentNode:
+            nextNode = currentNode.next
+            currentNode.next = prevNode
+            prevNode = currentNode
+            currentNode = nextNode
+
+        return prevNode
+        
