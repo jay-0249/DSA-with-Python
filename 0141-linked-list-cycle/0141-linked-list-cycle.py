c0a1f0a40cc31f24272c44ad81fb57e953020f88
jay-0249 <63956291+jay-0249@users.noreply.github.com>
2024-04-27 20:03:18 -0500

    Time: 38 ms (34.2%), Space: 19.2 MB (11.39%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        '''
+        1st approach  - using set
+            As we iterate, we check the nodes if it is in set else we add the node the node to the set
+            If the current node is in the set then it means we are visiting that node again. So we have a cycle in linked list.
+            Time Complexity - O(N)
+            Space complexity - O(N)
+
+        2nd approach - using two pointer (slow and fast)
+            As we iterate over the linked list, if the fast pointer meets the slow pointer, we have a cycle in the linked list
+            Time Complexity - O(N)
+            Space complexity - O(1)
+        '''
+        nodeSet = set()
+        currentNode = head
+        while currentNode:
+            if currentNode in nodeSet:
+                return True
+            else:
+                nodeSet.add(currentNode)
+            currentNode = currentNode.next
+        return False
