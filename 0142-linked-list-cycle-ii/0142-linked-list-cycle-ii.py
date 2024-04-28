8d7c55158ee4a776705ea92f1ecec16bb8f5e054
jay-0249 <63956291+jay-0249@users.noreply.github.com>
2024-04-27 20:51:32 -0500

    Time: 26 ms (84.05%), Space: 18.3 MB (13.21%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        nodeSet = set()
+        currentNode = head
+
+        while currentNode:
+            if currentNode in nodeSet:
+                return currentNode
+            else:
+                nodeSet.add(currentNode)
+            currentNode = currentNode.next
+
+        return None
