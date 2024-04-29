45ab871fc20bd1c673c6b2f70630c8a1484c1a9a
jay-0249 <63956291+jay-0249@users.noreply.github.com>
2024-04-28 21:57:59 -0500

    Time: 18 ms (78.13%), Space: 11.9 MB (64.66%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -10,24 +10,13 @@ class Solution(object):
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        '''
-        We do not have the node previous to the node to be deleted, we cannot delete this node by unlinking the node to be deleted.
-        Rather we can copy the next node val to the node to be deleted. We can skip next node in the linked list sequence
-        We do not need to move the values of all nodes - Optimisation
-        Time Complexity -> O(1)
-        Space Complexity -> O(1)
 
-        If we move the values of all nodes.
-        Time Complexity -> O(N)
-        Space Complexity -> O(1)
-        '''
-        currentNode = node
         #Check if the Node to the deleted is the Last Node
-        if currentNode.next:
+        if node.next:
                 #Move the value of next node to this node
-                currentNode.val = currentNode.next.val
+                node.val = node.next.val
                 #Skip the next node in the linked list sequence
-                currentNode.next = currentNode.next.next
+                node.next = node.next.next
 
         return
         
