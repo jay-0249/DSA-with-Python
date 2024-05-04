63b768f163c94974d8b242468a224a6223248f67
jay-0249 <63956291+jay-0249@users.noreply.github.com>
2024-05-03 22:46:16 -0500

    Time: 674 ms (23.52%), Space: 52.2 MB (35.39%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -16,19 +16,49 @@ class Solution(object):
         copy back the values into the nodes
         Time Complexity -> O(nlogn)
         Space Complexity -> O(n)
-        '''
-        nodeValues = []
-        currentNode = head
-        while currentNode:
-            nodeValues.append(currentNode.val)
-            currentNode = currentNode.next
-        
-        nodeValues.sort()
-        currentIndex = 0
-        currentNode = head
-        while currentNode:
-            currentNode.val = nodeValues[currentIndex]
-            currentNode = currentNode.next
-            currentIndex += 1
 
-        return head
+        2nd Approach
+        Use merge sorting
+        Break the linked list into two halfs, use recursion and follow merge sort
+        Again join the sorted halfs of linked lists
+        Time Complexity -> O(N)
+        '''
+
+        if head is None or head.next is None:
+            return head
+        
+        slowPointer = fastPointer = head
+        #Reach the middle node (odd of nodes)
+        #Reach the first middle node (even of nodes)
+        while fastPointer.next and fastPointer.next.next:
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next
+        
+        #Sort left half and right half of the linked lists seperately
+        rightHalf = self.sortList(slowPointer.next)
+        #Unlink the left half of the linked list with the right half of the linked list 
+        slowPointer.next = None
+        leftHalf = self.sortList(head)
+
+        #Merge the sorted linked lists
+        dummy = ListNode()
+        prevNode = dummy
+        while leftHalf and rightHalf:
+            if leftHalf.val < rightHalf.val:
+                prevNode.next = leftHalf
+                leftHalf = leftHalf.next
+            else:
+                prevNode.next = rightHalf
+                rightHalf = rightHalf.next
+            prevNode = prevNode.next
+        
+        if leftHalf:
+            prevNode.next = leftHalf
+        
+        if rightHalf:
+            prevNode.next = rightHalf
+
+        return dummy.next
+
+
+        
