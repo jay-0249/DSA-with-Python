1ddd309720a632e7dbeefd2372915d9f77d8dec5
jay-0249 <63956291+jay-0249@users.noreply.github.com>
2024-05-21 23:03:42 -0500

    Time: 26 ms (70.36%), Space: 12 MB (8.52%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -6,47 +6,23 @@ class Solution(object):
         :rtype: List[int]
         """
         '''
+        1st approach
         Sort both arrays
         Pick the smaller array, check each unique element of this array in the second array using binary search.
 
-        Time Complexity - O(NlogN)+O(MlogM)+O(MlogN)
+        Time Complexity - O(NlogN)+O(MlogM)+O(MlogN) ~ O(NlogN) where N is the length of the larger array
         Space Complexity - O(1)
+
+        2nd approach
+        We have to return only UNIQUE values of elements that are in both arrays
+        So we can use set data structure to store each array in a set and find their intersection
+        Time Complexity -> O(N+M) for creating sets, O(N)*O(1) for extracting intersection of the two sets. 
+        Finally the time complexity is ~ O(N) where N is the size of the larger array
+        Space Complexity -> O(N+M) for storing both arrays as sets
+        Finally the space complexity is ~ O(N) where N is the size of the larger array
         '''
-
-        #sort both arrays
-        nums1.sort()
-        nums2.sort()
-        
-        #Iterate over the smaller array and check if each unique number of the smaller array is present in the larger array. Binary Search is used to search.
-        def binarySearch(num, numsArr):
-            l, r = 0, len(numsArr)-1
-
-            while l<=r:
-                mid = l+(r-l)/2
-                if num == numsArr[mid]:
-                    return True
-                elif num < numsArr[mid]:
-                    r = mid-1
-                else:
-                    l = mid+1
-            
-            return False
-        
-        prevNum = None
-        intersectionOfArrays = []
-        #Shallow copy of an array will not create another copy of that array rather will also point to the same array
-        largerArray = copy.copy(nums1)
-        smallerArray = copy.copy(nums2)
-        if len(nums1) < len(nums2):
-            largerArray = copy.copy(nums2)
-            smallerArray = copy.copy(nums1)
-        
-        for i in range(0,len(smallerArray)):
-            currNum = smallerArray[i]
-            print(currNum, binarySearch(currNum,largerArray))
-            if currNum != prevNum and binarySearch(currNum,largerArray):
-                intersectionOfArrays.append(currNum)
-            prevNum = smallerArray[i]
-
-        return intersectionOfArrays
-
+        setNums1 = set(nums1)
+        setNums2 = set(nums2)
+        #we can also & operator in place of intersection
+        return setNums1 & setNums2
+        #return setNums1.intersection(setNums2)
