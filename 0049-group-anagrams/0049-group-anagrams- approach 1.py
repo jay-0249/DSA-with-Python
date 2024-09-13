16820a6a6bdc9fdc6eeae8193c11b9a0f551e051
Jay <nukalajayadeep999@gmail.com>
2024-09-13 17:47:26 -0500

    Time: 92 ms (14.54%), Space: 15.6 MB (81.99%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams- approach 1.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        resultDict = {}
+
+        for i in strs:
+            sortedStr = ''.join(sorted(i))
+            if sortedStr in resultDict:
+                resultDict[sortedStr].append(i)
+            else:
+                resultDict[sortedStr] = [i]
+
+        return list(resultDict.values())
