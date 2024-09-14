8079947a21c80b393deaea317283f46b880be30c
Jay <nukalajayadeep999@gmail.com>
2024-09-14 13:50:12 -0500

    Time: 141 ms (9.35%), Space: 19 MB (29.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,21 +7,30 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        
+        '''
+        Using arrays instead of the heaps
+        Time Complexity O(N) optimised from that of heaps that process in the order of O(N+UlogU+klogU)
+        Space Complexity O(N) tradeoff for faster processing compared to heaps that requires O(U) order space
+        '''
         #create a hashmap of unique numbers occuring in the list and their frequency
         freqMap = {}
         for i in nums:
             currFreq = freqMap.get(i,0)
             freqMap[i] = 1 + currFreq
         
-        #We need a data structure to sort the frequencies faster and also access the top k frequent elements
-        tupleList = [(-freq,val) for val,freq in freqMap.items()]
-        
-        #Transform the list of tuples as heap
-        heapq.heapify(tupleList)
-        
-        #Extract the top k frequent elements
-        result = [heapq.heappop(tupleList)[1] for i in range(0,k)]
+        #Now create 2D list, to store all the values having a frequency at a index equals to the frequency. Max Freq can be 'len(nums)' when all the array elements are same. Minimum frequency can be '1'
+        freqEle2DList = [[] for i in range(len(nums)+1)]
 
+        #iterate over the dictionary to fill the 2D list
+        for val,freq in freqMap.items():
+            freqEle2DList[freq].append(val)
+
+        #Now over the 2D list from back to get top k frequent elements
+        result = []
+        for i in range(len(nums),0,-1):
+            for j in freqEle2DList[i]:
+                result.append(j)
+                if len(result) == k:
+                    return result
+        
         return result
-    
