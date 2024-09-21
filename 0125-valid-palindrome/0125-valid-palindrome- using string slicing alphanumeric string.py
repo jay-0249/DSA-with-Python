69089e79ef29cc4a1515dc8ef127254cbfc6eed0
Jay <nukalajayadeep999@gmail.com>
2024-09-21 23:19:04 +0530

    Time: 385 ms (7.08%), Space: 12.7 MB (72.05%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome- using string slicing alphanumeric string.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        #convert the string into lowercase alphanumeric string
+        alpha_numeric_string = ""
+        for i in s:
+            if i.isalnum():
+                alpha_numeric_string += i.lower()
+        
+        #verify if the alphanumeric string is a palindrome using string slicing
+        return alpha_numeric_string[::1] == alpha_numeric_string[::-1]
+
+        
