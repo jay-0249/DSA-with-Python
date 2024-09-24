fddc3752eac34957ed7f0ff94341260bfce74cfb
Jay <nukalajayadeep999@gmail.com>
2024-09-25 02:09:58 +0530

    Time: 1017 ms (20%), Space: 22 MB (97.27%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water- greedy approach - best.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        '''
+        Bruteforce Approach - nested loop
+            Iterate over the array, for each iteration fix that number as the first wall and then iterate over the rest of array to find a wall that can be the second wall, such that the volume is maximised. In each iteration, update the maximum volume of the container.
+            Time Complexity - O(N^2)
+            Space Complexity - O(1)
+        
+        maxVolume = 0    
+        for i in range(0,len(height)-1):
+            for j in range(i+1,len(height)):
+                currentVolume = abs((j-i)*(min(height[j],height[i])))
+                maxVolume = max(maxVolume, currentVolume)
+        return maxVolume 
+
+        Greedy Approach - Two Pointers
+        The basic underlying idea is that at each point of time, I want to to move to a bigger container possible locally.
+        First using two pointers from left and right side of the array. Now that you have a container, check if the volume is greater than max volume, if not update it.
+        Then, the wall that is the smaller wall, move to another wall
+        '''
+        maxVolume = 0
+        l, r = 0,len(height)-1
+        while l < r:
+            if height[l] < height[r]:
+                maxVolume = max(maxVolume, height[l]*(r-l))
+                l += 1
+            else:
+                maxVolume = max(maxVolume, height[r]*(r-l))
+                r -= 1
+        
+        return maxVolume
