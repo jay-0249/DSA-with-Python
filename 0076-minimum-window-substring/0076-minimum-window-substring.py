9964e55bbf0d5ada74e8fcacbc82397346e37dd3
Jay <nukalajayadeep999@gmail.com>
2024-10-01 17:02:49 +0530

    Time: 374 ms (23.82%), Space: 15.2 MB (19.39%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,3 +1,4 @@
+from collections import Counter
 class Solution(object):
     def minWindow(self, s, t):
         """
@@ -6,60 +7,58 @@ class Solution(object):
         :rtype: str
         """
         '''
-        Create a dictionary of the elements in the string 't' with their respective frequency.
-        Use a counter as a count of all elements of 't'.
-        Now using two pointers begin, end. Slide through the string 's' to find a substring between start and end pointers such that it contains all the elements of 't' (matching with the element repetition as that of 't')
-        You can check this using a counter(size of 't'), whenever you find a matching element in 's', check if we were still required to find such an element in string 's' (dictT[char]>0), decrement the local counter. (also decrement the dictT[char])
-        So when local counter is zero, it means that we have found a substring that has all elements as that of string 't'
-        Now let's try to check if the begining character is an element in string 't' or not. If not, increment the begin pointer
-        '''
+        find a substring of s -> such that contain all characters & repititions of string t
+        substring, slide over an iterable -> sliding window with variable size
 
-        #check if 's' contains atleast equal or more than number of characters to that of string 't'
+        first make a hash map of frequency of characters in string t, using Counter data structure
+
+        also initialise a counter variable representing the number of unique characters and repititions are still required
+        
+        intialise start and end(current) pointers for the sliding window
+
+        slide over the string, for each iteration check if the character at the current pointer is required
+
+        if freq of the current character is zero, decrement the counter
+
+        if the counter is zero, update the length of the minimum substring and minimum substring
+
+        while the counter is zero, decrement the start pointer and update the frequency and counter variable
+
+        return the shortest substring of s such that it contains all characters and repititions of t
+        '''
+        #check for the edge case
         if len(s) < len(t):
             return ""
-        #create a result sub-string
-        resultSubstring = ""
 
-        #create a dictionary of elements in string 't' and their respective frequency
-        dictTelements = dict()
-        for ele in t:
-            #if dictTelements.get(ele):    it is better to use 'in operator' bcz if dictTelements.get(ele) fails when get() returns '0'. Eventhough is not a possibility in our case, better to follow 'in' operator
-            if ele in dictTelements:
-                dictTelements[ele] = dictTelements[ele]+1 
-            else:
-                dictTelements[ele] = 1
+        #initialise start pointer of the window
+        start = 0
         
-        #start sliding through the string 's'
-        begin, end = 0, 0
-        counterS = len(t)
+        #initialise the frequency and counter variables
+        freq_dict = Counter(t)
+        counter = len(freq_dict)
 
-        #Until the end pointer is in bounds of string 's'
-        while end < len(s):
-            #as we do not have scenario that string 't' can be an empty string, we do not check if the counter is zero prior initially rather we can check if the current character is a character that is in 't'
-            currentChar = s[end]
-            #check if this char is still required to make it a substring that contains all characters of string 't'
-            if dictTelements.get(currentChar) > 0:
-                counterS -= 1
-            #check if this char is in dictionary
-                #there can be a case where we can the frequency of currentChar (in dictionary) is zero or less than zero, which means we do not require that character to make the substring of 's' a required substring. But we still have to account for that element for sliding window(if that element is present as an element of string 't') and thereby decrease the frequency of the element. so check if this char is in dictionary
-            if currentChar in dictTelements:
-                dictTelements[currentChar] = dictTelements[currentChar] - 1
+        #initialise result variables for the worst case
+        shortest_substring = str("")
+        length_shortest_substring = len(s)+1
 
-            #Increase the window right end by moving the right end to the next character
-            end += 1
-
-            #Check if the substring between pointers 'begin' and 'end' contains all elements of 't'
-            while counterS == 0 :
-                #check if this substring is smaller to that of the previous shortest substring that conatins all elements of 't'
-                if end - begin < len(resultSubstring) or resultSubstring == "":
-                    resultSubstring = s[begin:end:1]
-                #Check if the begin element is in dictTelements
-                #As we move to slide the window from 'begin' to next element, we will have to find such an element again in the string 's' to be a substring that contains all elements of string 't'
-                if s[begin] in dictTelements:
-                    dictTelements[s[begin]] += 1
-                    if dictTelements[s[begin]]>0:
-                        counterS += 1
-                
-                begin += 1
+        for current in range(0,len(s)):
+            #update freq_dict, counter based on the current character
+            if s[current] in freq_dict:
+                freq_dict[s[current]] -= 1
+                if freq_dict[s[current]] == 0:
+                    counter -= 1
             
-        return resultSubstring
+            while counter == 0:
+                #update the result if required based on current iteration
+                length_current_substring = current-start+1
+                if length_current_substring < length_shortest_substring:
+                    length_shortest_substring = length_current_substring
+                    shortest_substring = s[start:current+1]
+                #update start pointer
+                if s[start] in freq_dict:
+                    if freq_dict[s[start]] == 0:
+                        counter += 1
+                    freq_dict[s[start]] += 1
+                start += 1
+        
+        return shortest_substring
