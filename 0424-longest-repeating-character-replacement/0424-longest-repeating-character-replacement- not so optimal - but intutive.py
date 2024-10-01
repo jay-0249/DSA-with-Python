17aecf96fe4a7f2b035acf28143c132fc237b17a
Jay <nukalajayadeep999@gmail.com>
2024-10-01 14:26:31 +0530

    Time: 73 ms (56.65%), Space: 12.1 MB (44.27%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement- not so optimal - but intutive.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        '''
+        find longest substring
+        sliding window with variable length
+        start, current pointers
+        as len(s) = 1,
+        in the worst case answer, the longest substring can be of length 1
+        similarly freq_max_repeated_char can be 1 in the worst case
+        At each point, we see the frequency of the current character, increment the character count, 
+        WE WILL UPDATE the max frequency by calculating the maximum of the frequency in the frequency dictionary. This will add the Time Complexity to O(N) per each iteration.
+        If length - maximum frequency is greater than k, then we have a case where we might need more than k replacements
+        so increment the start pointer, decrement the start element frequency
+        this way our substring is always in compliance with the maximum k repititions rule
+        we do not need to worry about the freq_max_repeated_char, as if we find a case when we have a greater frequency, we will update freq_max_repeated_char
+        also in each iteration we will check if we have found a longer string and update accordingly
+        
+        Time Complexity - O(N^2)
+        Space Complexity - O(N)
+        '''
+
+        length_longest_string = 0
+        start = 0
+
+        freq_max_repeated_char = 0
+        freq_dict = {}
+
+        for current in range(0,len(s)):
+            freq_dict[s[current]] = 1 + freq_dict.get(s[current], 0)
+            #adds the time complexity of O(N) in each case
+            freq_max_repeated_char = max(freq_dict.values())
+
+            if ((current-start+1) - freq_max_repeated_char) > k:
+                freq_dict[s[start]] -= 1
+                start += 1
+
+            length_longest_substring = max(current-start+1, length_longest_string)
+        
+        return length_longest_substring
