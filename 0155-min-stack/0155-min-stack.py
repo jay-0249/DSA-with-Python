6dfb9b2d78151f8c76e8eab93301c6a47e81d42e
Jay <nukalajayadeep999@gmail.com>
2024-10-30 15:22:17 -0500

    Time: 8 ms (64.14%), Space: 15.2 MB (74.54%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -9,7 +9,7 @@ use minStack to store min value encountered until now. we can avoid storing min 
 class MinStack(object):
     
     def __init__(self):
-        self.mainStack = deque()
+        self.stack = deque()
         self.minStack = deque() 
 
     def push(self, val):
@@ -17,26 +17,23 @@ class MinStack(object):
         :type val: int
         :rtype: None
         """
-        self.mainStack.append(val)
+        self.stack.append(val)
         if not self.minStack or self.minStack[-1] >= val:
             self.minStack.append(val)
-        else:
-            self.minStack.append(self.minStack[-1])
 
     def pop(self):
         """
         :rtype: None
         """
-        if self.mainStack:
+        if self.stack and self.stack.pop() == self.minStack[-1]:
             self.minStack.pop()
-            return self.mainStack.pop()
    
     def top(self):
         """
         :rtype: int
         """
-        if self.mainStack:
-            return self.mainStack[-1]
+        if self.stack:
+            return self.stack[-1]
         
     def getMin(self):
         """
