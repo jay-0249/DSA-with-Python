6846620830ae77127c560176bd20f248bdc996a6
Jay <nukalajayadeep999@gmail.com>
2024-10-30 15:00:17 -0500

    Time: 6 ms (74.5%), Space: 15.1 MB (74.54%) - LeetHub

--- 0155-min-stack/0155-min-stack- stack -deque - brute force method- code optimised.py
+++ 0155-min-stack/0155-min-stack- stack -deque - brute force method- code optimised.py
@@ -1,61 +1,49 @@
+from collections import deque
+'''
+Brute Force:-
+use minstack to store min value encountered until now. so at each time we insert a element, we insert the min value encountered until now.
+
+Optimisation:- 
+use minStack to store min value encountered until now. we can avoid storing min value for each node in stack to avoid duplicates until if the min number is encountered more than once. This will improve the space complexity in practical use.
+'''
 class MinStack(object):
-
-    '''
-    Here we have to implement a custom MinStack class.
-    The push, pop, top operations can be performed in the order of O(1) time complexity with a stack.
     
-    But how to implement a getMin() function that retrives the minimum of all the numbers at any point in the order of O(1) time complexity?
-
-    what if we maintain another stack data structure whose top would always be the minimum of all the numbers inserted until now including the current number,
-    if an element from main_stack is popped then the top of this min_stack will also be popped as the top of min_stack represents the min of all numbers in min_stack including the current number popped
-    now the next top of min_stack would be the minimum of all the numbers in the main_stack currently 
-    ...
-    ...
-    this way until when each element is popped from main_stack, the min_stack will also have to be popped so that the top of min_stack will be the minimum of all the numbers of main_stack at any point of time
-
-    when a number is inserted, we can compare if it is less than minimum of previous numbers inserted. If yes, insert this number else insert the previous minimum. In this way we can calculate the minimum at each insert in O(1) time.
-
-    similarly pop the min_stack when main_stack is popped
-    '''
-
     def __init__(self):
-        self.stack = []
-        self.min_stack = []
+        self.mainStack = deque()
+        self.minStack = deque() 
 
     def push(self, val):
         """
         :type val: int
         :rtype: None
         """
-        self.stack.append(val)
-        #calculate the minimum of the numbers with the current value included. This will be minimum of current value and previous minimum of all the numbers appended into the stack previously
-        min_value_including_current_value = min(val, self.min_stack[-1] if self.min_stack else val)
-        #understand the above calculation of min at each push operation with bottom to top approach, i.e, with just one or two elements to all elements
-        self.min_stack.append(min_value_including_current_value)
-        
+        self.mainStack.append(val)
+        if not self.minStack or self.minStack[-1] >= val:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(self.minStack[-1])
 
     def pop(self):
         """
         :rtype: None
         """
-        self.stack.pop()
-        #pop the minimum value as if of a window that contains all the numbers till & including the popped element
-        self.min_stack.pop()
-        #after the pop operation the top element of the min_stack would be the minimum of all the numbers in the stack
-
+        if self.mainStack:
+            self.minStack.pop()
+            return self.mainStack.pop()
+   
     def top(self):
         """
         :rtype: int
         """
-        return self.stack[-1]
-
+        if self.mainStack:
+            return self.mainStack[-1]
+        
     def getMin(self):
         """
         :rtype: int
         """
-        return self.min_stack[-1]
-        
-
+        if self.minStack:
+            return self.minStack[-1]
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
