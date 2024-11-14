4566317d2f6369edfe7e102d2be892341d594ae1
Jay <nukalajayadeep999@gmail.com>
2024-11-14 13:37:11 -0600

    Time: 0 ms (100%), Space: 11.6 MB (67.86%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,16 +7,23 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+        
+        if n == 0:
+            return
+
+        #minElement = nums1[0] if nums1[0] < nums2[0] else nums2[0]
+        
         b = n-1
         a = m-1
         k = m+n-1
 
         while b>-1:
-            if nums2[b] >= nums1[a]:
+            if a>-1 and nums2[b]<nums1[a]:
+                nums1[k] = nums1[a]
+                #nums1[a] = minElement
+                a -= 1
+            else:
                 nums1[k] = nums2[b]
                 b -= 1 
-            else:
-                nums1[k] = nums1[a]
-                a -= 1
             k -= 1
-        return 
+        return
