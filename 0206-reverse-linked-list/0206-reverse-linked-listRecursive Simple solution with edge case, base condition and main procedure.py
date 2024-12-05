80334ea1bbc988335d9761007ff75df5a415dfc7
Jay <nukalajayadeep999@gmail.com>
2024-12-05 12:17:20 -0600

    Time: 0 ms (100%), Space: 14.1 MB (19.26%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-listRecursive Simple solution with edge case, base condition and main procedure.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        #EdgeCases
+        if head == None:
+            return head
+        
+        #Base Condition
+        prevNode = None
+        currNode = head
+        nextNode = currNode.next
+
+        #first node reversal
+        currNode.next = prevNode
+
+        #Recursive until your nextNode is None
+        while nextNode:
+            #move current node to next node, accordingly nextNode and prevNode are moved
+            prevNode = currNode
+            currNode = nextNode
+            nextNode = nextNode.next
+            #Link the currNode's next node to prevNode
+            currNode.next = prevNode
+        
+        return currNode
+
+        
