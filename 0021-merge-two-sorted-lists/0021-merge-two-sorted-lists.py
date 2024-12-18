a8055688412e97906e2bafe0bfc3143fc4eac40c
Jay <nukalajayadeep999@gmail.com>
2024-12-17 19:51:03 -0600

    Time: 2 ms (27.19%), Space: 12.6 MB (15.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,33 +10,34 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        #Check if any list is empty
+        #I want to merge a list into the other list
+        #so I will check if either list 1 or list 2 is empty
         if list1 is None:
             return list2
-        if list2 is None:
+        elif list2 is None:
             return list1
+        
+        #Now list1 None or list2 None or both empty case are handled
+        #Now I will compare list2 head with list1 head, if list2 head element is lesser than I will consider list2 as my primary list
+        primaryList, secondaryList = list1, list2
 
-        head = None
-        #Assign the head
-        if list1.val < list2.val:
-            head = list1
-            list1 = list1.next
-        else:
-            head = list2
-            list2 = list2.next
-
-        #prevNode will be helpful to link node with next smallest number
-        prevNode = head
-        #prevNode will always not be None. Check if list1 node or list2 node is None.
-        while list1 and list2:
-            if list1.val<list2.val:
-                prevNode.next = list1
-                list1 = list1.next
+        if list2.val < list1.val:
+            primaryList, secondaryList = list2, list1
+        
+        primaryListHead = primaryList
+        
+        #If current head of secondary node is less than the next node of the primary list, insert the current secondary head node as the next node of the primary Node
+        while primaryList.next and secondaryList:
+            if secondaryList.val < primaryList.next.val:
+                temp1 = primaryList.next
+                primaryList.next = secondaryList
+                secondaryList = secondaryList.next
+                primaryList = primaryList.next
+                primaryList.next = temp1
             else:
-                prevNode.next = list2
-                list2 = list2.next
-            prevNode = prevNode.next
-            
-        prevNode.next = list1 if list1 else list2
-
-        return head
+                primaryList = primaryList.next
+        
+        if primaryList.next is None:
+            primaryList.next = secondaryList
+        
+        return primaryListHead
