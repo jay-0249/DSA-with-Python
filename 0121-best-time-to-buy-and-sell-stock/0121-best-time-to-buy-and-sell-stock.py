23cb04b1b9fba0079acb76d58572df7aad11e5a4
Jay <nukalajayadeep999@gmail.com>
2026-03-29 20:24:59 -0500

    Time: 167 ms (5.46%), Space: 22.6 MB (9.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,20 +4,25 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        
+        #Sliding Window, the profit for each adjacent days may summed to get window profit. Calculate max of such window profits
+        #create an array of adjacent day profits
+
+        adj_profit = []
+
+        for i in range(0, len(prices)-1):
+            adj_profit.append(prices[i+1] - prices[i])
+
+        left = 0
+        profit = 0
         max_profit = 0
-        start, end = 0, 0
 
-        while end < len(prices):
-            profit = prices[end]-prices[start]
+        for right in range(0, len(adj_profit)):
+            while profit < 0:
+                profit -= adj_profit[left]
+                left += 1
 
-            end += 1
-
-            if max_profit < profit:
-                max_profit = profit
+            profit += adj_profit[right]
+            max_profit = max( max_profit, profit)
             
-            if profit<=0:
-                start = end - 1
-
-        return max_profit
         
+        return max_profit
